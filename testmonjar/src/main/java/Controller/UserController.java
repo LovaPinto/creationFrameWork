90628d0b7dd38d9035cfd8a02758529497a9752b
LovaPinto <lovasoa.alexis@gmail.com>
2026-07-12 18:04:53 +0300

    add sprint 4

--- testmonjar/src/main/java/Controller/UserController.java
+++ testmonjar/src/main/java/Controller/UserController.java
@@ -1,14 +1,24 @@
 package Controller;
 
 import com.lovapinto.MyController;
+import com.lovapinto.Model;
+import com.lovapinto.ModelAndView;
 import com.lovapinto.UrlMapping;
+import Repository.UserRepository;
+
+import java.util.List;
 
 @MyController
 public class UserController {
-    @UrlMapping(path="/login")
-    public void login(HttpServletRequest req, HttpServletResponse res) { }
 
-    @UrlMapping(path="/register")
-    public void register(HttpServletRequest req, HttpServletResponse res) { }
- 
+    private final UserRepository userRepository = new UserRepository();
+
+    @UrlMapping(path = "/users")
+    public ModelAndView list(Model model) {
+        List<String> users = userRepository.findAll();
+        model.setAttribute("users", users);
+
+        return new ModelAndView("users/list")
+                .setModelContainer(model);
+    }
 }
